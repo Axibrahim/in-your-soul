584d09c08ad54f826631c0733e80713a23b3105a
amr ibrahim <amro29945@gmail.com>
2026-06-24 21:35:11 +0300

    Update mobile responsiveness and checkout UI

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'freks-jungle-system-lime-secret-2025'
+    SECRET_KEY = os.environ.get('BASE_SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         f'sqlite:///{os.path.join(basedir, "freks.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
