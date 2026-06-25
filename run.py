0c0aa950e5d66ead313e8f6c09882ccd2c35e5b3
amr ibrahim <amro29945@gmail.com>
2026-06-25 23:51:09 +0300

    Setting adminstartors

--- run.py
+++ run.py
@@ -17,25 +17,24 @@ def make_shell_context():
     }
 
 
-with app.app_context():
-    from app.models import User
-
-    if not User.query.filter_by(username="admin").first():
-        admin = User(
-            first_name="amr",
-            last_name="ibrahim",
-            username="bahz",
-            is_admin=True
-        )
-
-        admin.set_password("Amro4488348")
-
-        db.session.add(admin)
-        db.session.commit()
-
-
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+
+        if not User.query.filter_by(username="bahz").first():
+            admin = User(
+                first_name="Amr",
+                last_name="Ibrahim",
+                username="bahz",
+                is_admin=True
+            )
+
+            admin.set_password("Amro4488348")
+
+            db.session.add(admin)
+            db.session.commit()
+
+            print("Admin account created")
+
     app.run(debug=True, host='0.0.0.0', port=5000)
