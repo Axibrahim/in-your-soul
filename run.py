24f1cf3f56ccdb61877ddef3e0e3bb2544bf3cb6
amr ibrahim <amro29945@gmail.com>
2026-06-25 19:16:04 +0300

    setting admins

--- run.py
+++ run.py
@@ -16,6 +16,25 @@ def make_shell_context():
         'Address': Address,
     }
 
+
+with app.app_context():
+    from app.models import User
+
+    if not User.query.filter_by(username="admin").first():
+        admin = User(
+            first_name="amr",
+            last_name="ibrahim",
+            username="bahz",
+            is_admin=True
+        )
+
+        admin.set_password("Amro4488348")
+
+        db.session.add(admin)
+        db.session.commit()
+
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
